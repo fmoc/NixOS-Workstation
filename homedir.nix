783525b3f06f3d0031791722893e711e6e441463
Markus <ciapa@lewd.wtf>
2023-09-16 19:19:33 +0200

    Actually exit homedir activation script

--- homedir.nix
+++ homedir.nix
@@ -3,7 +3,7 @@
     source = ./home;
   };
   system.activationScripts.script.text = ''
-    if [[ $(who | grep fablab) > 0 ]]; then echo logged in; fi
+    if [[ $(who | grep fablab) > 0 ]]; then exit 0; fi
     cp -r /etc/fablab/ /home
     chown -R fablab:users /home/fablab
     chmod -R u+w /home/fablab
