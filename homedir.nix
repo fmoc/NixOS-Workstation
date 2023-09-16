b960f5a2e325a019ab7c9aa3bb114001ebe9e751
Markus <ciapa@lewd.wtf>
2023-09-16 17:16:00 +0200

    Avoid updating homedir while user is logged in
    This prevents some bugs, like Firefox crashing due to overwriting the profile.
    
    Usually, we change the profile directly, anyway, so updating it isn't necessary.
    
    In case we do want to update the profile, logging out the current user (and running `nixos-rebuild switch` directly as root) will update the home dir nevertheless.
    
    Related to #12

--- homedir.nix
+++ homedir.nix
@@ -3,6 +3,7 @@
     source = ./home;
   };
   system.activationScripts.script.text = ''
+    if [[ $(who | grep fablab) > 0 ]]; then echo logged in; fi
     cp -r /etc/fablab/ /home
     chown -R fablab:users /home/fablab
     chmod -R u+w /home/fablab
