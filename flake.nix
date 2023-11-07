6fb78ff2920f8a12c2f54954ba50b0e59b1d1637
Markus <ciapa@lewd.wtf>
2023-11-07 16:39:59 +0000

    Add inkstitch as flake source

--- flake.nix
+++ flake.nix
@@ -1,11 +1,16 @@
 {
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    inkstitch = {
+      url = "git+https://codeberg.org/tropf/nix-inkstitch";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, inkstitch }: {
     nixosConfigurations.fablab = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      _module.args = { inherit inkstitch; };
       modules = [ ./configuration.nix ];
     };
   };
