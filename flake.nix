c6daf9395dfbbde7c61e5f2661ca5bcba23f6c8b
Markus <ciapa@lewd.wtf>
2023-11-07 18:26:57 +0000

    Don't use upstream nixpkgs for inkstitch

--- flake.nix
+++ flake.nix
@@ -1,10 +1,7 @@
 {
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
-    inkstitch = {
-      url = "git+https://codeberg.org/tropf/nix-inkstitch";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    inkstitch = { url = "git+https://codeberg.org/tropf/nix-inkstitch"; };
   };
 
   outputs = { self, nixpkgs, inkstitch }: {
