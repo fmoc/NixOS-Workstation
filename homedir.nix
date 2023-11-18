e766cdf41bb4ebb4045482c01bc6f17166101b82
Bernd Stellwag <burned@zerties.org>
2023-11-18 11:47:22 +0100

    add .visicut from separate git repo on installation

--- homedir.nix
+++ homedir.nix
@@ -2,9 +2,17 @@
   environment.etc.fablab = {
     source = ./home;
   };
+  environment.etc.visicut-settings = {
+    source = builtins.fetchGit {
+      url = "https://git.fablab-altmuehlfranken.de/fablab/visicut-settings.git";
+      ref = "main";
+    };
+  };
   system.activationScripts.script.text = ''
     if [[ $(who | grep fablab) > 0 ]]; then exit 0; fi
     cp -r /etc/fablab/ /home
+    rm -rf /home/fablab/.visicut
+    cp -r /etc/visicut-settings /home/fablab/.visicut
     chown -R fablab:users /home/fablab
     chmod -R u+w /home/fablab
   '';
