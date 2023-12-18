cc41cb8d5e8a59ac9262553e0b4766a0f1233933
Markus <ciapa@lewd.wtf>
2023-12-18 19:22:09 +0100

    Automatically collect garbage before updating system
    ```
    [...]
    91603 store paths deleted, 141106.99 MiB freed
    ```

--- autoupdate.nix
+++ autoupdate.nix
@@ -6,6 +6,7 @@
     wantedBy = [ "multi-user.target" ];
     script = ''
       if [ -f /autoupdate-finished ]; then exit 0; fi
+      nix-collect-garbage --delete-older-than 30d
       rm -rf /etc/nixos
       git clone --depth=1 https://github.com/FabLab-Altmuehlfranken/NixOS-Workstation.git /etc/nixos
       nixos-rebuild boot --flake '/etc/nixos#fablab'
